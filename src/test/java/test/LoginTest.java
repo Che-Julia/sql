6b62fcff52ca3b7c880a99956648e6f1cd1004fd
JuliaChe <chebykina.yulya28@bk.ru>
2023-05-17 12:09:14 +0300

    Initial commit

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import data.DataHelper;
+import data.SQLHelper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class LoginTest {
+    @AfterAll
+    static void teardown(){
+        SQLHelper.cleanDatabase();
+    }
+    @Test
+    void shouldSuccessfulLogin(){
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+
+    }
+}
